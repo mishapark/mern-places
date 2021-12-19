ec3c15f6dc50a50ca97a6a444831f28c792ac4a1
mishapark <66741910+mishapark@users.noreply.github.com>
2021-12-18 23:08:07 -0500

    first

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Emprie",
+    description: "Skyscrapper",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th, New York",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emprie",
+    description: "Skyscrapper",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th, New York",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = (props) => {
+  const userId = useParams().uid;
+
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,18 @@
+import React from "react";
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+      name: "Max",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
